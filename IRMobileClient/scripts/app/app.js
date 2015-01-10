4cb5129553b738e156a0dd1499987d4d3053db3d
Sean Bennett <contact@caniluk.com>
2015-01-11 12:49:56 +1300

    All dependencies initialized and working

---
+++ IRMobileClient/scripts/app/app.js
@@ -0,0 +1,17 @@
+﻿(function() {
+    var irApp = angular.module('irApp', ['ngCordova', 'ionic']);
+
+    irApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'views/home.html'
+            })
+            .state('settings', {
+                url: '/',
+                templateUrl: 'views/settings.html'
+            });
+    }]);
+})();
