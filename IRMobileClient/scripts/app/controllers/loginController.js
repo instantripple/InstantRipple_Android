91d84b0568aadceba2e39eb3f48419cc762d91d9
Sean Bennett <contact@caniluk.com>
2015-01-11 15:08:11 +1300

    Add RippleTrade credentials login

--- IRMobileClient/scripts/app/controllers/loginController.js
+++ IRMobileClient/scripts/app/controllers/loginController.js
@@ -2,8 +2,21 @@
     var irApp = angular.module('irApp');
 
     irApp.controller('loginController', [
-        '$state', 'clientSession',
-        function ($state, clientSession) {
+        '$scope', '$state', 'clientSession',
+        function ($scope, $state, clientSession) {
+            $scope.loginForm = {};
+
+            $scope.login = function() {
+                var vaultClient = new ripple.VaultClient();
+                vaultClient.loginAndUnlock($scope.loginForm.username, $scope.loginForm.password, null, function(err, res) {
+                    if (err) {
+                        return;
+                    }
+
+                    clientSession.start(res.username, res.blob.data.account_id, res.secret);
+                    $state.go('home');
+                });
+            }
         }
     ]);
 })();
