3f6a93557b4d1834d94485b146ebecfdf28feac7
Sean Bennett <contact@caniluk.com>
2015-01-11 19:19:12 +1300

    Add ripple remote connections

---
+++ IRMobileClient/scripts/app/services/rippleRemote.js
@@ -0,0 +1,52 @@
+﻿(function () {
+    var irApp = angular.module('irApp');
+
+    irApp.factory('rippleRemote', function () {
+        var remote = new ripple.Remote({
+            servers: {
+                host: "s-west@ripple.com",
+                port: 443,
+                secure: true,
+            },
+            trace: true,
+            trusted: true,
+            local_signing: true,
+            local_fee: true,
+            fee_cushion: 1.1,
+            max_fee: 100000
+        });
+
+        var requestAccountInfo = function(address) {
+            remote.requestAccountInfo({
+                account: address,
+                ledger: "validated"
+            }, function(err, res) {
+                if (err) {
+                    throw err;
+                }
+                return {
+                    balance: parseFloat(res.account_data.Balance / 1000000)
+                };
+            });
+        };
+
+        var requestAccountLines = function(address) {
+            remote.requestAccountLines({
+                account: address,
+                ledger: "validated"
+            }, function(err, res) {
+                if (err) {
+                    throw err;
+                }
+                return {
+                    lines: res.lines
+            }
+            });
+        };
+
+        return {
+            getAccountInfo: requestAccountInfo,
+            getAccountLines: requestAccountLines
+        };
+    });
+})();
