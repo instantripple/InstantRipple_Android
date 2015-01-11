839680c2ae71d5c2db4865c570278356b927530e
Sean Bennett <contact@caniluk.com>
2015-01-11 15:12:12 +1300

    Add login bypass function to speed testing

--- IRMobileClient/scripts/app/controllers/loginController.js
+++ IRMobileClient/scripts/app/controllers/loginController.js
@@ -16,7 +16,13 @@
                     clientSession.start(res.username, res.blob.data.account_id, res.secret);
                     $state.go('home');
                 });
-            }
+            };
+
+            // Used to bypass login for testing. Secret will not be valid.
+            $scope.bypass = function() {
+                clientSession.start('SeanBennett', 'rBM2aVcC3ecy6swYsZFxTShsWRAvC7rCJa', '');
+                $state.go('home');
+            };
         }
     ]);
 })();
