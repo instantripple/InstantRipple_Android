409a40e6be69b1df2ee08fd6fae69001d80afdd6
Sean Bennett <contact@caniluk.com>
2015-01-13 11:26:35 +1300

    Add currency filter to customize precision by asset

---
+++ IRMobileClient/scripts/app/filters/currencyFilter.js
@@ -0,0 +1,18 @@
+﻿(function() {
+    var irApp = angular.module('irApp');
+
+    irApp.filter('currency', ['$filter',
+        function($filter) {
+            return (function (amount, currency) {
+                var decimals = 2;
+                switch (currency) {
+                    case 'BTC':
+                        decimals = 4;
+                        break;
+                };
+
+                return $filter('number')(amount, decimals);
+            });
+        }
+    ]);
+})();
