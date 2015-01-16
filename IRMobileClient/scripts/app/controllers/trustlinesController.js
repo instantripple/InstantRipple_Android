891443f90dfc953227dca51658fb817c1aceece4
Sean Bennett <contact@caniluk.com>
2015-01-17 12:31:17 +1300

    Display trustlines

--- IRMobileClient/scripts/app/controllers/trustlinesController.js
+++ IRMobileClient/scripts/app/controllers/trustlinesController.js
@@ -2,9 +2,29 @@
     var irApp = angular.module('irApp');
 
     irApp.controller('trustlinesController', [
-        '$scope', 'rippleRemote', 'clientSession',
-        function ($scope, rippleRemote, clientSession) {
-            
+        '$scope', 'rippleRemote', 'clientSession', '$ionicLoading',
+        function ($scope, rippleRemote, clientSession, $ionicLoading) {
+            $scope.trustlines = {};
+
+            $scope.trustlines.update = function () {
+                $ionicLoading.show();
+                rippleRemote.getAccountLines(clientSession.session().address, function(err, res) {
+                    var lines = res.lines;
+                    var linesByCurrency = Enumerable.From(lines)
+                        .GroupBy('$.currency', null, function(key, x) {
+                            return {
+                                currency: key,
+                                lines: Enumerable.From(lines).Where(function(y) { return y.currency == key; }).ToArray(),
+                            };
+                        }).ToArray();
+                    $scope.$apply(function() {
+                        $scope.trustlines.linesByCurrency = linesByCurrency;
+                        $ionicLoading.hide();
+                    });
+                });
+            };
+
+            $scope.trustlines.update();
         }
     ]);
 })();
